bb97dead803cf30c8d87164799ea30eeaadf38fc
May0Need5 <128439825+May0Need5@users.noreply.github.com>
2024-04-23 13:42:12 -0400

    Add files via upload

---
+++ pop.js
@@ -0,0 +1,17 @@
+function showModalPopUp() {
+
+    var popupWindow = window.open("video.html", "ModalPopUp", "width=960,height=540", "left=200", "top=50");
+
+    popupWindow.focus();
+   
+    };
+
+    function showModalPopUp2() {
+	
+	var popupWindow2 = window.open("https://drive.google.com/file/d/1h_fEET4WOfCEUOko0YrCMK6hFFU93-0c/view", "ModalPopUp", "left=200", "top=50");
+	
+	popupWindow2.focus();
+	
+	};
+
+
