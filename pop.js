160cab3440a72b5e175b5028eeaf45ee93e6144e
May0Need5 <128439825+May0Need5@users.noreply.github.com>
2024-05-24 00:29:23 -0400

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,6 +1,6 @@
 function showModalPopUp() {
 
-    var popupWindow = window.open("video.html", "ModalPopUp", "left=200", "top=50");
+    var popupWindow = window.open("video.html", "ModalPopUp", "width=800", "height=500" "left=200", "top=50");
 
     popupWindow.focus();
    
